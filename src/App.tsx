716c170de71ace028725f1e26ee2f6a7e76d63fe
willamy388720 <willamygt@gmail.com>
2023-08-29 02:50:16 -0300

    pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+
+import { Router } from "./Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Hello, World!</h1>
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { Timer, Scroll } from "@phosphor-icons/react";
+
+import { HeaderContainer } from "./styles";
+
+import logoIgnite from "../../assets/logo-ignite.svg";
+import { NavLink } from "react-router-dom";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
+}
